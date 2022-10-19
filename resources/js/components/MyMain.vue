478bff7af6799b0f24711feb0bb7ce863618dfc6
saidmoussadek <saiddus24@gmail.com>
2022-10-19 02:50:40 +0200

    push creazione controller ,link, e componenti

---
+++ resources/js/components/MyMain.vue
@@ -0,0 +1,31 @@
+<template>
+  
+<div class='container'>
+
+
+</div>
+
+
+
+</template>
+
+<script>
+    export default {
+            name:'MyMain',
+            methods:{
+                getPosts(){
+                    axios.get('/api/posts')
+                    .then((response)=>{
+                        console.log(response);
+                    });
+                }
+            },
+            mounted(){
+                this.getPosts();
+            }
+    }
+</script>
+
+<style>
+
+</style>
